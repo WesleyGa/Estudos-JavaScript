8b9e17d532e5b23902b0ca83872210bbc37273fa
WesleyGa <wes16062002@gmail.com>
2024-07-17 16:52:38 -0300

    Exercício de fixação: Ordenação de Array

--- Fixacao/array.js
+++ Fixacao/array.js
@@ -4,8 +4,8 @@ function ordenarArray(array) {
     for (let i = 0; i < t - 1; i++) { // percorre o comprimento do array e cada vez que 'i' aumenta, significa que mais um elemento foi colocado na posição correta no final do array.
         console.log("I = " + i)
 
-        for (let j = 0; j < t - i - 1; j++) { //  diminui a quantidade de comparação de acordo com o loop externo,
-                                              //  porque os ultimos elementos (i) ja estão em ordem.
+        for (let j = 0; j < t - i - 1; j++) { /*  diminui a quantidade de comparação de acordo com o loop externo,
+                                                  porque os ultimos elementos (i) ja estão em ordem. */
             console.log("J = " + j)
             // Lógica da troca
             if (array[j] > array[j + 1]) {
